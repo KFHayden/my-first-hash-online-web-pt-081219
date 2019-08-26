2175090010b6057a532794a892c7d891aa9bcdba
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-26 05:11:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,6 @@ end
 
 
 def shipping_manifest
-
   my_hash = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
@@ -14,7 +13,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+  shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
